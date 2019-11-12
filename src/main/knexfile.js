b29e85859cffcfe23515101de00b06901bde551f
Karthik9321 <karthikramachandra93@gmail.com>
2019-11-12 20:20:03 +0530

    refactor

--- src/main/knexfile.js
+++ src/main/knexfile.js
@@ -1,9 +1,16 @@
 /* eslint-disable import/no-extraneous-dependencies */
+const path = require('path');
+
+require('dotenv').config({
+  path: path.resolve(__dirname, '..', '..', '.env'),
+});
+
 const Config = require('./config');
 
 const databaseConfig = Config.get('database')
   .get('postgres')
   .toJS();
+
 module.exports = {
   development: databaseConfig,
   staging: databaseConfig,
